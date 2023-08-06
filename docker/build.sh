393690dd821b4688e53b8d76991e382996b5c00d
David Schoegler <david.schoegler@gmail.com>
2023-08-06 12:07:51 +0200

    added

--- docker/build.sh
+++ docker/build.sh
@@ -3,10 +3,10 @@ USER_NAME="build"
 PROJECT="toeziOS"
 LAYER="toezios"
 
-BUILD_INPUT_DIR="/home/$USER_NAME/input"
-BUILD_OUTPUT_DIR="/home/$USER_NAME/output"
+BUILD_INPUT_DIR="/home/$USER_NAME/start"
+BUILD_OUTPUT_DIR="/home/$USER_NAME/output/"
+#rm -rf $BUILD_OUTPUT_DIR/build
 mkdir -p $BUILD_INPUT_DIR $BUILD_OUTPUT_DIR
-
 cd $BUILD_INPUT_DIR
 git clone --recurse-submodules https://github.com/toezi/toeziOS.git
 
@@ -15,5 +15,5 @@ TEMPLATECONF=$BUILD_INPUT_DIR/$PROJECT/meta-$LAYER/custom/
 export TEMPLATECONF
 
 
-source $BUILD_INPUT_DIR/$PROJECT/poky/oe-init-build-env \
+source $BUILD_INPUT_DIR/$PROJECT/poky/oe-init-build-env $BUILD_OUTPUT_DIR \
     && bitbake $LAYER-image
