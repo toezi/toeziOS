89e2fc6021be5778e0497c798106af75d0784108
David Schoegler <david.schoegler@gmail.com>
2023-08-06 13:58:29 +0200

    first commit

--- docker/build.sh
+++ docker/build.sh
@@ -4,11 +4,23 @@ PROJECT="toeziOS"
 LAYER="toezios"
 
 BUILD_INPUT_DIR="/home/$USER_NAME/"
+BUILD_OUTPUT_TMP_DIR="/home/$USER_NAME/output_tmp/"
 BUILD_OUTPUT_DIR="/home/$USER_NAME/output/"
+
+mkdir -p $BUILD_INPUT_DIR $BUILD_OUTPUT_TMP_DIR $BUILD_OUTPUT_DIR
 
 cd $BUILD_INPUT_DIR/$PROJECT
 
+git pull --recurse-submodules
+
+rm -rf $BUILD_OUTPUT_TMP_DIR/*
+cp -r $BUILD_OUTPUT_DIR/*  $BUILD_OUTPUT_TMP_DIR/*
+
 TEMPLATECONF=$BUILD_INPUT_DIR/$PROJECT/meta-$LAYER/custom/ \
-    source poky/oe-init-build-env $BUILD_OUTPUT_DIR \
+    source poky/oe-init-build-env $BUILD_OUTPUT_TMP_DIR \
     && bitbake toezios-image
+
+rm -rf $BUILD_OUTPUT_DIR/*
+cp -r $BUILD_OUTPUT_TMP_DIR/*  $BUILD_OUTPUT_DIR/*
+
 
