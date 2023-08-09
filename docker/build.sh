7631d64cdd447906b741b7a2895c135a2df25c8c
David Schoegler <david.schoegler@gmail.com>
2023-08-09 22:49:17 +0200

    first commit

--- docker/build.sh
+++ docker/build.sh
@@ -13,10 +13,14 @@ cd $BUILD_INPUT_DIR/$PROJECT
 
 git pull --recurse-submodules
 
+cp -r $BUILD_OUTPUT_DIR/../downloads/* $BUILD_OUTPUT_DIR_TMP/../downloads/
+cp -r $BUILD_OUTPUT_DIR/../sstate-cache/* $BUILD_OUTPUT_DIR_TMP/../sstate-cache/
 
 TEMPLATECONF=$BUILD_INPUT_DIR/$PROJECT/meta-$LAYER/conf/templates/default \
     source poky/oe-init-build-env $BUILD_OUTPUT_DIR_TMP \
     && bitbake toezios-image --runall=fetch
+
+cp -r $BUILD_OUTPUT_DIR_TMP/../* $BUILD_OUTPUT_DIR/../
     
 
 
